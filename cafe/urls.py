b7b5c89e5d5b4002a51ba5c59b7096a686a36b2b
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-07-24 09:42:19 +0000

    Add url path to edit booking html

--- cafe/urls.py
+++ cafe/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.SpecialsView.as_view(), name='home'),
     path('add_booking/', views.add_booking, name='add_booking'),
     path('view_booking/', views.view_booking, name='view_booking'),
+    path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
 ]
