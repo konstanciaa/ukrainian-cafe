2c60c4b1f3b99ad2e9a79185d41bc3033fa7ac03
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-07-24 14:43:56 +0000

    Add url path to add_specials html

--- cafe/urls.py
+++ cafe/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('view_booking/', views.view_booking, name='view_booking'),
     path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
     path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
+    path('add_specials/', views.add_specials, name='add_specials'),
 ]
