a429806de2df7bc7ce6c1d8f0f00cf1c5f841810
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-07-24 12:14:19 +0000

    Add url path to delete booking html

--- cafe/urls.py
+++ cafe/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add_booking/', views.add_booking, name='add_booking'),
     path('view_booking/', views.view_booking, name='view_booking'),
     path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
 ]
